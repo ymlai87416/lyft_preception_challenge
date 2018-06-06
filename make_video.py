893aaf28e0d4357436f81c7cbc63d1a4cc71342d
ymlai87416 <ymlai87416@gmail.com>
2018-06-06 21:03:48 +0800

    first commit

--- make_video.py
+++ make_video.py
@@ -1,81 +1,103 @@
-import os
-from glob import glob
-import scipy
-import numpy as np
+import sys, skvideo.io, json, base64
 from PIL import Image
-import scipy.misc
+from io import BytesIO
+import tensorflow as tf
+import sys
+import numpy as np
+import scipy
+import os
+import cv2
+from tqdm import tqdm
 
-def make_video(outvid, images, fps=5, size=None,
-               is_color=True, format="XVID"):
-    """
-    Create a video from a list of images.
- 
-    @param      outvid      output video
-    @param      images      list of images to use in the video
-    @param      fps         frame per second
-    @param      size        size of each frame
-    @param      is_color    color
-    @param      format      see http://www.fourcc.org/codecs.php
-    @return                 see http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_gui/py_video_display/py_video_display.html
- 
-    The function relies on http://opencv-python-tutroals.readthedocs.org/en/latest/.
-    By default, the video will have the size of the first image.
-    It will resize every image to this size before adding them to the video.
-    """
-    from cv2 import VideoWriter, VideoWriter_fourcc, imread, resize
-    fourcc = VideoWriter_fourcc(*format)
-    vid = None
-    for image in images:
-        if not os.path.exists(image):
-            raise FileNotFoundError(image)
-        img = imread(image)
-        if vid is None:
-            if size is None:
-                size = img.shape[1], img.shape[0]
-            vid = VideoWriter(outvid, fourcc, float(fps), size, is_color)
-        if size[0] != img.shape[1] and size[1] != img.shape[0]:
-            img = resize(img, size)
-        vid.write(img)
-    vid.release()
-    return vid
-
-def save(video_img_dir, video_fname):
-    command = "ffmpeg -r 1 -i {0}/img%01d.png -vcodec mpeg4 -y {1}" % (video_img_dir, video_fname)
-    os.system(command)
+file = sys.argv[1]
+outfile = sys.argv[2]
 
 
-def preprocess(input_img, input_label, video_img_dir):
-    for image_fn in glob(os.path.join(input_img, '*.png')):
-        base_file_name = os.path.basename(image_fn)
-        pred_label_fn = os.path.join(input_label, base_file_name)
+if file == 'demo.py':
+    print("Error loading video")
+    quit
 
-        image = scipy.misc.imread(image_fn)
-        label = scipy.misc.imread(pred_label_fn)[:, :, 0:1]
 
-        binary_car_result = label == 10
+def get_model_path(model_name):
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), model_name)
 
-        binary_road_result = label == 7
 
-        # Apply road judgement to original image as a mask with alpha = 50%
+# Define encoder function
+def encode(array):
+    retval, buffer = cv2.imencode('.png', array)
+    return base64.b64encode(buffer).decode("utf-8")
+
+
+def crop_image(image):
+    return image[274:530, :, :]
+
+
+def pad_image(image):
+    result = np.zeros((600, 800))
+    result[274:530, :] = image
+    return result
+
+
+video = skvideo.io.vread(file)
+
+writer = skvideo.io.FFmpegWriter(outfile, inputdict={'-r': '10'}, outputdict={'-vcodec': 'mpeg4', '-b': '2000k'})
+
+answer_key = {}
+
+# Frame numbering starts at 1
+frame = 1
+
+graph_file = './submissions/final/deeplab_10_trim_xception_a.opt.h5.pb'
+image_shape = (256, 800)
+
+use_xla = False
+
+config = tf.ConfigProto()
+if use_xla:
+    jit_level = tf.OptimizerOptions.ON_1
+    config.graph_options.optimizer_options.global_jit_level = jit_level
+
+with tf.Session(graph=tf.Graph(), config=config) as sess:
+    gd = tf.GraphDef()
+    g = sess.graph
+    with tf.gfile.Open(graph_file, 'rb') as f:
+        data = f.read()
+        gd.ParseFromString(data)
+    tf.import_graph_def(gd, name='')
+    x = g.get_tensor_by_name('input_1:0')
+    out = g.get_tensor_by_name('output_node0:0')
+
+    for rgb_frame in video:
+        image = crop_image(rgb_frame)
+
+        image = image / 127.5 - 1.
+        im_softmax = sess.run(
+            out,
+            {x: [image]})
+        num_classes = im_softmax.shape[3]
+        im_softmax = im_softmax.reshape(image_shape[0], image_shape[1], num_classes)
+
+        im_argmax = np.argmax(im_softmax, axis=2).reshape(image_shape[0], image_shape[1])
+        im_argmax = pad_image(im_argmax)
+
+        # Look for red cars :)
+        binary_car_result = np.where(im_argmax == 10, 1, 0).astype('uint8').reshape(600, 800, 1)
+
+        # Look for road :)
+        binary_road_result = np.where(im_argmax == 7, 1, 0).astype('uint8').reshape(600, 800, 1)
+
         mask_road = np.dot(binary_road_result, np.array([[0, 255, 0, 127]]))
         mask_car = np.dot(binary_car_result, np.array([[0, 0, 255, 127]]))
 
         mask_road = scipy.misc.toimage(mask_road, mode="RGBA")
         mask_car = scipy.misc.toimage(mask_car, mode="RGBA")
 
-        street_img = Image.fromarray(image)
+        street_img = Image.fromarray(rgb_frame)
         street_img.paste(mask_road, box=None, mask=mask_road)
         street_img.paste(mask_car, box=None, mask=mask_car)
+        writer.writeFrame(np.array(street_img))
 
-        street_img.save(os.path.join(video_img_dir, base_file_name))
+        # Increment frame
+        frame += 1
 
-
-input_img = './data/Test/CameraRGB'
-input_label = './deeplab_pascal/runs/1528159274.8549316'
-video_img_dir = './video'
-video_fname = 'example.mp4'
-
-preprocess(input_img, input_label, video_img_dir)
-
-
-save(video_img_dir, video_fname)
+writer.close()
---
+++ make_video_from_image.py
@@ -0,0 +1,81 @@
+import os
+from glob import glob
+import scipy
+import numpy as np
+from PIL import Image
+import scipy.misc
+
+def make_video(outvid, images, fps=5, size=None,
+               is_color=True, format="XVID"):
+    """
+    Create a video from a list of images.
+ 
+    @param      outvid      output video
+    @param      images      list of images to use in the video
+    @param      fps         frame per second
+    @param      size        size of each frame
+    @param      is_color    color
+    @param      format      see http://www.fourcc.org/codecs.php
+    @return                 see http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_gui/py_video_display/py_video_display.html
+ 
+    The function relies on http://opencv-python-tutroals.readthedocs.org/en/latest/.
+    By default, the video will have the size of the first image.
+    It will resize every image to this size before adding them to the video.
+    """
+    from cv2 import VideoWriter, VideoWriter_fourcc, imread, resize
+    fourcc = VideoWriter_fourcc(*format)
+    vid = None
+    for image in images:
+        if not os.path.exists(image):
+            raise FileNotFoundError(image)
+        img = imread(image)
+        if vid is None:
+            if size is None:
+                size = img.shape[1], img.shape[0]
+            vid = VideoWriter(outvid, fourcc, float(fps), size, is_color)
+        if size[0] != img.shape[1] and size[1] != img.shape[0]:
+            img = resize(img, size)
+        vid.write(img)
+    vid.release()
+    return vid
+
+def save(video_img_dir, video_fname):
+    command = "ffmpeg -r 10 -i {0}/%06d.png -vcodec mpeg4 -b 2000k -y {1}" % (video_img_dir, video_fname)
+    os.system(command)
+
+
+def preprocess(input_img, input_label, video_img_dir):
+    for image_fn in glob(os.path.join(input_img, '*.png')):
+        base_file_name = os.path.basename(image_fn)
+        pred_label_fn = os.path.join(input_label, base_file_name)
+
+        image = scipy.misc.imread(image_fn)
+        label = scipy.misc.imread(pred_label_fn)[:, :, 0:1]
+
+        binary_car_result = label == 10
+
+        binary_road_result = label == 7
+
+        # Apply road judgement to original image as a mask with alpha = 50%
+        mask_road = np.dot(binary_road_result, np.array([[0, 255, 0, 127]]))
+        mask_car = np.dot(binary_car_result, np.array([[0, 0, 255, 127]]))
+
+        mask_road = scipy.misc.toimage(mask_road, mode="RGBA")
+        mask_car = scipy.misc.toimage(mask_car, mode="RGBA")
+
+        street_img = Image.fromarray(image)
+        street_img.paste(mask_road, box=None, mask=mask_road)
+        street_img.paste(mask_car, box=None, mask=mask_car)
+
+        street_img.save(os.path.join(video_img_dir, base_file_name))
+
+
+input_img = './data/Test/CameraRGB'
+input_label = './deeplab_pascal/runs/1528159274.8549316'
+video_img_dir = './video'
+video_fname = 'example.mp4'
+
+preprocess(input_img, input_label, video_img_dir)
+
+
+save(video_img_dir, video_fname)
